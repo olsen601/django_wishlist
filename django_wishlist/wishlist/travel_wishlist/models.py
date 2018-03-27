eca354d61682f4667a340e8f1aaf18ab6317c491
olsen601 <tedssmith2556@gmail.com>
2018-03-27 11:48:11 -0500

    lab 8

--- django_wishlist/wishlist/travel_wishlist/models.py
+++ django_wishlist/wishlist/travel_wishlist/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Create a data object to store information about the places
 
 class Place(models.Model):
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    visited_date = models.DateTimeField(blank=True, null=True)
+    note = models.TextField(default='')
+
+    #updates the date visited to the current time, later it will accept user input to set the date it was visited
+    def visit(self):
+        self.visited_date = timezone.now()
+        self.save()
+
 
     def __str__(self):
         return '%s visited? %s' % (self.name, self.visited)
